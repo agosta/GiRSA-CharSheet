4eb8f7bb614b9912b06d3b7b24fad633029763d8
Giovanni Agosta <agosta@acm.org>
2020-06-02 20:46:06 +0200

    [Bugfix]

--- converter.py
+++ converter.py
@@ -165,7 +165,7 @@ def ods_to_pdf(filename):
 	# Call XeLaTeX
 	import subprocess
 	import os
-        print(os.getcwd())
+	print(os.getcwd())
         x=subprocess.call(['xelatex', fname])
 	if x : print('Error compiling LaTeX file {}: {}'.format(fname,x))
 	return x
